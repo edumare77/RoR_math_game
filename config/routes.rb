fb27741c8de2e2a97076f22a10bfcf7b23385b48
Edu <edumare77@gmail.com>
2016-10-13 23:41:18 +0100

    login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+  get 'sessions/new'
   get '/about',   to: 'welcome#about'
   get  '/signup',  to: 'users#new'
-  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
